55b2e1f85793a547844716d44295e294314be762
pollirey <psmirnova9@yandex.ru>
2026-09-07 14:16:32 +0300

    Добавление стилей

--- js/index.js
+++ js/index.js
@@ -4,3 +4,17 @@ let data = await getAllData();
 
 console.log(data);
 
+const films = document.getElementById("filmsContainer");
+const search = document.getElementById("searchInput");
+
+async function renderFilms(films) {
+    
+};
+
+async function filterFilms(search) {
+    
+}
+
+// у нас будет renderFilms которая принимает фильмы, и еще для поиска filterFilms
+//  которая принимает то что мы вводим в инпут их 
+// просто создать код внутри писать не надо мы его на занятии сделаем
