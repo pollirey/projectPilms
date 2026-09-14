44698b11390c1ae2474414abf008bcc8e769e40b
pollirey <psmirnova9@yandex.ru>
2026-09-14 15:57:06 +0300

    Добавление стилей на новой странице

---
+++ js/clienthall.js
@@ -0,0 +1,12 @@
+
+const filmName = document.getElementById("film-name");
+const seanceTime = document.getElementById("seance-time");
+const hallName = document.getElementById("hall-name");
+const hallContainer = document.getElementById("hall-container");
+const seatsContainer = document.getElementById("seats-container");
+const standartPrice = document.getElementById("standart-price");
+const vipCount = document.getElementById("vip-count");
+const vipPrice = document.getElementById("vip-price");
+const standartCount = document.getElementById("standart-count");
+const totalPrice = document.getElementById("total-price");
+const bookBtn = document.getElementById("book-btn");
